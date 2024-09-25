765f6d7328c11a213401c2a0cc0b3f85c92b720c
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-25 11:09:27 +0300

    Add accounts url

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -22,5 +22,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("to_do_management.urls", namespace="management")),
 ]
