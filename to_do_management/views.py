2fa7c00b65fef1948fa4d835e3a35868c5e5afd0
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-25 11:19:50 +0300

    Add empty generic

--- to_do_management/views.py
+++ to_do_management/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from to_do_management.forms import TaskForm
 from to_do_management.models import Task, Tag
 
 
@@ -11,6 +12,30 @@ class TaskListView(generic.ListView):
     template_name = "to_do_management/home.html"
 
 class TaskCreateView(generic.CreateView):
+    form_class = TaskForm
     model = Task
     template_name = "to_do_management/task_form.html"
     success_url = reverse_lazy("management:home")
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+
+
+
+class TagListView(generic.ListView):
+    pass
+
+class TagCreateView(generic.CreateView):
+    pass
+
+class TagDeleteView(generic.DeleteView):
+    pass
+
+class TagDetailView(generic.DetailView):
+    pass
+
+class TagUpdateView(generic.UpdateView):
+    pass
+
