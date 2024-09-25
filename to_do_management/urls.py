d53d2f638a2bf4956b0f68626208af72829ffb3d
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-25 10:51:09 +0300

    Add create view for task

--- to_do_management/urls.py
+++ to_do_management/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from to_do_management.views import index
+from to_do_management.views import TaskListView, TaskCreateView
 
 app_name = "management"
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", TaskListView, name="home"),
+    path("tasks/create/", TaskCreateView, name="task-create"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- to_do_management/views.py
+++ to_do_management/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from to_do_management.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
-    pass
+    model = Task
+    paginate_by = 10
+    template_name = "to_do_management/home.html"
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    template_name = "to_do_management/task_form.html"
+    success_url = reverse_lazy("management:home")
