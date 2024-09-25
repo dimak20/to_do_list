9decb24a8641e99133bb92883840dbfcc326fb2e
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-25 11:40:50 +0300

    Add form and modify base.html

--- to_do_management/forms.py
+++ to_do_management/forms.py
@@ -2,10 +2,20 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from to_do_management.models import Task
+from to_do_management.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local", "class": "form-control"}
+        )
+    )
     class Meta:
         model = Task
         fields = "__all__"
--- to_do_management/urls.py
+++ to_do_management/urls.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from to_do_management.views import TaskListView, TaskCreateView
+from to_do_management.views import TaskListView, TaskCreateView, TaskDetailView, TaskUpdateView, TaskDeleteView
 
 app_name = "management"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
