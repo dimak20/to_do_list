ed278ab0c3c576dbf62bc120e6575359b82fc4b9
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-25 11:41:40 +0300

    Create absolute url method

--- to_do_management/models.py
+++ to_do_management/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=63, unique=True)
@@ -16,3 +18,6 @@ class Task(models.Model):
 
     def __str__(self):
         return self.content
+
+    def get_absolute_url(self):
+        return reverse('management:task-detail', args=[str(self.id)])
